0b67b645b9dd7adcd17a81aa6ee68a1f61623477
semctrlz <vagner.lenon@gmail.com>
2020-04-10 16:58:41 -0300

    adicionada a porta

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ require('dotenv/config');
 module.exports = {
 dialect:  process.env.DB_DIALECT,
 host: process.env.DB_HOST,
+port: process.env.DB_PORT,
 username: process.env.DB_USER,
 password: process.env.DB_PASS,
 database: process.env.DB_NAME,
