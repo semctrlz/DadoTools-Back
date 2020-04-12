f05bcc96afd8228ce18399ba034804f5b2af7d13
semctrlz <vagner.lenon@gmail.com>
2020-04-12 13:43:09 -0300

    disable cors

--- src/app.js
+++ src/app.js
@@ -18,12 +18,7 @@ class App{
   }
 
   cors(){
-    this.server.use(cors(
-      {
-        origin: process.env.NODE_ENV === 'development'?process.env.HOST:'https://dadotools.netlify.com',
-        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-      }
-    ));
+    this.server.use(cors());
   }
 
   routes(){
