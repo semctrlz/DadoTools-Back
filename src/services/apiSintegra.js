3a6ca9d22c613378cc6a74078bb5d2337f4b5008
semctrlz <vagner.lenon@gmail.com>
2021-04-29 12:14:18 -0300

    sintegra 01

--- src/services/apiSintegra.js
+++ src/services/apiSintegra.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiSintegra = axios.create({
-  baseURL: 'https://www.sintegraws.com.br/',
+  baseURL: 'https://www.sintegraws.com.br',
 });
 
 export default apiSintegra;
